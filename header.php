fcf5130e5fd767710e2ac7877574229924530ecf
Alex <Chavet>
2014-07-08 09:11:14 +1000

    Meta

--- header.php
+++ header.php
@@ -1,7 +1,9 @@
 
 <html>
 	<head>
+		
 		<title> Flexbox - Code Challenge</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link rel="icon" 
 					type="image/png" 
 						href="img/note.png">
